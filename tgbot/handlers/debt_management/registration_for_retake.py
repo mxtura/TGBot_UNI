97e8ff7a7cddf10cbfd17beef34e057a6c2ccb9c
Turkin Max <mxturaa@gmail.com>
2023-11-21 16:04:58 +0300

    Исправление неисправности кнопки

--- tgbot/handlers/debt_management/registration_for_retake.py
+++ tgbot/handlers/debt_management/registration_for_retake.py
@@ -6,20 +6,17 @@ import tgbot.keyboards.inline as ikb
 import tgbot.keyboards.reply as rkb
 from tgbot.misc.states import LoginChangeStates
 from tgbot.filters.user_type import UserTypeFilter
-from tgbot.handlers.cancel import cq_cancel_all
 from tgbot.models.database_instance import db
 
-logger = logging.getLogger(__name__)
 reg_retake_request_text = "Пожалуйста, напишите название предмета, который хотите пересдать:"
 
 async def send_reg_retake_request(message: Message, state: FSMContext):
     """Просит ввести название предмета"""
-    logger.info(reg_retake_request_text)
     del_msg = await message.answer(text=reg_retake_request_text,
                                    reply_markup=rkb.retake_input_cancel_keyboard)
     await state.update_data(del_msg=del_msg)
 
 
 def register_reg_retake(dp):
-    dp.register_message_handler(send_reg_retake_request, ~UserTypeFilter("student"), content_types=['text'],
+    dp.register_message_handler(send_reg_retake_request, UserTypeFilter("student"), content_types=['text'],
                                 text=['Записаться на пересдачу'])
